8f7eb153eac5d44c163d93cce3cab130e436f7db
YashMankumareKH <yash.mankumare.cmaug25@gmail.com>
2026-01-26 17:47:22 +0530

    Gender Field added in Register DTO

--- backend/src/main/java/com/project/entities/Professional.java
+++ backend/src/main/java/com/project/entities/Professional.java
@@ -24,6 +24,7 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Entity
 @Table(
@@ -34,15 +35,14 @@ import lombok.Setter;
 )
 @Getter
 @Setter
+@ToString
 public class Professional extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long professionalId;
 
-    /**
-     * User account linked to this professional
-     */
+    
     @OneToOne(fetch = FetchType.LAZY , cascade = CascadeType.ALL)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
