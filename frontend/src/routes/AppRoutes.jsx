25acbe4c4820fc2cda296e8e6fc09243a97eb7bf
sarthaks55 <sarthak.sambare.ss@gmail.com>
2026-01-29 19:41:17 +0530

    approutes changes

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -3,7 +3,6 @@ import Login from "../auth/Login";
 import Register from "../auth/Register";
 import RegisterUser from "../auth/RegisterUser";
 import RegisterProfessional from "../auth/RegisterProfessional";
-//import UserDashboard from "./pages/user/UserDashboard";
 import ProtectedRoute from "./ProtectedRoute";
 import Profile from "../pages/user/Profile";
 import UserDashboard from "../pages/user/UserDashboard";
@@ -29,89 +28,56 @@ import Home from "../pages/Home";
 import AboutUs from "../pages/AboutUs";
 import Services from "../pages/Services";
 
-
-
-
 const AppRoutes = () => {
   return (
     <Routes>
       {/* Public Routes */}
-       <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/about" element={<AboutUs />} /> {/* Add AboutUs route */}
       <Route path="/services" element={<Services />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="/register-user" element={<RegisterUser />} />
       <Route path="/register-professional" element={<RegisterProfessional />} />
-
-      {/* <Route
-          path="/user/dashboard"
-          element={
-            <ProtectedRoute role="ROLE_USER">
-              <UserDashboard />
-            </ProtectedRoute>
-          }
-        /> */}
-        {/* <Route index element={<DashboardHome />} /> */}
-        {/* <Route path="user/profile" element={<Profile />} /> */}
-        {/* <Route path="appointments" element={<Appointments />} />
-        <Route path="mood" element={<Mood />} />
-        <Route path="diary" element={<Diary />} /> */}
-
-        <Route path="/user" element={<UserDashboard />}>
-          <Route index element={<DashboardHome />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="appointments" element={<Appointments />} />
-          <Route path="book" element={<BookAppointment />} />
-          <Route path="moods" element={<MoodDashboard />} />
-          <Route path="diary" element={<DiaryDashboard />} />
-          <Route path="notifications" element={<Notifications />} />
-        </Route>
-
-        
-        {/* <Route path="/professional" element={
-          <ProtectedRoute role="ROLE_PROFESSIONAL">
-            <ProfessionalDashboard />
-          </ProtectedRoute> } >
+      <Route path="/user" element={<UserDashboard />}>
         <Route index element={<DashboardHome />} />
-          <Route path="profile" element={<Profile />} />
-          <Route path="appointments" element={<Appointments />} />
-          <Route path="availability" element={<Availability />} />
-        </Route> */}
-        
-        <Route path="/professional" element={
+        <Route path="profile" element={<Profile />} />
+        <Route path="appointments" element={<Appointments />} />
+        <Route path="book" element={<BookAppointment />} />
+        <Route path="moods" element={<MoodDashboard />} />
+        <Route path="diary" element={<DiaryDashboard />} />
+        <Route path="notifications" element={<Notifications />} />
+      </Route>
+      <Route
+        path="/professional"
+        element={
           <ProtectedRoute role="ROLE_PROFESSIONAL">
             <ProfessionalDashboard />
-          </ProtectedRoute> } >
-  <Route index element={<ProfessionalHome />} />
-  <Route path="profile" element={<ProfessionalProfile />} />
-  <Route path="appointments" element={<ProfessionalAppointments />} />
-  <Route path="availability" element={<ProfessionalAvailability />} />
-  <Route path="notifications" element={<Notifications />} />
-
-
-</Route>
-
-<Route
-  path="/admin"
-  element={
-    <ProtectedRoute role="ROLE_ADMIN">
-      <AdminDashboard />
-    </ProtectedRoute>
-  }
->
-  <Route index element={<AdminDashboardHome />} />
-  <Route path="profile" element={<AdminProfile />} />
-  <Route path="users" element={<Users />} />
-  <Route path="professionals" element={<Professionals />} />
-  <Route path="appointments" element={<AdminAppointments />} />
-  <Route path="notifications" element={<Notifications />} />
-</Route>
-
-
-
+          </ProtectedRoute>
+        }
+      >
+        <Route index element={<ProfessionalHome />} />
+        <Route path="profile" element={<ProfessionalProfile />} />
+        <Route path="appointments" element={<ProfessionalAppointments />} />
+        <Route path="availability" element={<ProfessionalAvailability />} />
+        <Route path="notifications" element={<Notifications />} />
+      </Route>
+      <Route
+        path="/admin"
+        element={
+          <ProtectedRoute role="ROLE_ADMIN">
+            <AdminDashboard />
+          </ProtectedRoute>
+        }
+      >
+        <Route index element={<AdminDashboardHome />} />
+        <Route path="profile" element={<AdminProfile />} />
+        <Route path="users" element={<Users />} />
+        <Route path="professionals" element={<Professionals />} />
+        <Route path="appointments" element={<AdminAppointments />} />
+        <Route path="notifications" element={<Notifications />} />
+      </Route>
     </Routes>
-    
   );
 };
 
